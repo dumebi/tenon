6285ee19b1bf5df78c07b005594254752e67be13
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2023-09-04 17:43:21 +0100

    implement home page sections except gallery section

---
+++ src/components/Layout/Partners.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Partners.module.css";
+import partner1 from "../../assets/partners/image 1.png";
+import partner2 from "../../assets/partners/image 2.png";
+import partner3 from "../../assets/partners/image 3.png";
+import partner4 from "../../assets/partners/image 4.png";
+import partner5 from "../../assets/partners/image 5.png";
+import partner6 from "../../assets/partners/image 6.png";
+import partner7 from "../../assets/partners/image 7.png";
+
+function Partners() {
+  const partners = [
+    { img: partner4 },
+    { img: partner1 },
+    { img: partner5 },
+    { img: partner6 },
+    { img: partner3 },
+    { img: partner2 },
+    { img: partner7 },
+  ];
+  return (
+    <div className={styles.partners}>
+      <div className={styles.partners_header}>
+        <h1>Our Partners:</h1>
+      </div>
+      <div className={styles.partners_list}>
+        {partners.map((data, index) => (
+          <div key={index} className={styles.partners_image}>
+            <div className={styles.partners_image_wrapper}>
+              <img src={data.img} alt="partners" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Partners;
--- src/components/Sections/HomeBody.js
+++ src/components/Sections/HomeBody.js
@@ -11,6 +11,7 @@ import procurementIcon from "../../assets/svg/Product-procurement.svg";
 import haulageIcon from "../../assets/svg/Haulage.svg";
 import warehouseIcon from "../../assets/svg/warehousing.svg";
 import Button from "../UI/Button";
+import Partners from "../Layout/Partners";
 
 const tenon_about = [
   {
@@ -97,9 +98,49 @@ const rateOptions = [
   "MSC",
 ];
 
+const faqData = [
+  {
+    question: "How long does it take to ship my order?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  {
+    question: "How long does it take to ship my order?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  {
+    question: "How long does it take to ship my order?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "Orders are usually shipped within 1-2 business days after placing the order.",
+  },
+  // Add more FAQ items as needed
+];
+
 function HomeBody() {
   const [activeShippingRate, setActiveShippingRate] = useState("container");
   const [activeOptionRate, setActiveOptionRate] = useState(0);
+  const [openQuestion, setOpenQuestion] = useState(null);
 
   const handleParagraphClick = (paragraphId) => {
     setActiveShippingRate(paragraphId);
@@ -107,6 +148,15 @@ function HomeBody() {
 
   const handleRateOptionIndex = (index) => {
     setActiveOptionRate(index);
+  };
+
+  // Function to handle question clicks
+  const handleQuestionClick = (index) => {
+    if (openQuestion === index) {
+      setOpenQuestion(null);
+    } else {
+      setOpenQuestion(index);
+    }
   };
 
   return (
@@ -301,6 +351,131 @@ function HomeBody() {
             <Button>See more</Button>
           </div>
         </div>
+      </div>
+      {/* partners */}
+      <Partners />
+
+      {/* data driven section */}
+
+      <div className={`${styles.hero} ${styles.body2}`}>
+        <div className={styles.body_about_parent}>
+          <div className={styles.body_about}>
+            <div>
+              <h1>We are a data driven organization</h1>
+              <p>
+                With a relentless focus on efficiency, reliability, and
+                innovation, we specialize in optimizing warehouses and logistics
+                operations to drive unparalleled success for businesses of all
+                sizes.
+              </p>
+            </div>
+          </div>
+          <div className={styles.body_about_cards}>
+            <Card className={styles.data_card}>
+              <h1>30 billion</h1>
+              <p>Total cargo handled</p>
+            </Card>
+            <Card className={styles.data_card}>
+              <h1>$59,000,000</h1>
+              <p>Value of cargo handled</p>
+            </Card>
+            <Card className={styles.data_card}>
+              <h1>Total hours -</h1>
+              <p>LTI</p>
+            </Card>
+            <Card className={styles.data_card}>
+              <h1>30 billion</h1>
+              <p>HSE metrics</p>
+            </Card>
+          </div>
+        </div>
+      </div>
+      {/* FAQ's */}
+      <div className={`${styles.hero} ${styles.body}`}>
+        <div className={styles.body_about_parent}>
+          <div className={styles.body_about}>
+            <div>
+              <h1>Frequently asked questions</h1>
+              <p>
+                With a relentless focus on efficiency, reliability, and
+                innovation, we specialize in optimizing warehouses and logistics
+                operations to drive unparalleled success for businesses of all
+                sizes.
+              </p>
+            </div>
+          </div>
+          <div className={styles.faq}>
+            {faqData.map((data, index) => (
+              <div
+                className={styles.faq_list}
+                key={index}
+                onClick={() => handleQuestionClick(index)}
+              >
+                <div className={styles.faq_header}>
+                  <h1>{data.question}</h1>
+                  {openQuestion === index ? (
+                    <svg
+                      onClick={() => handleQuestionClick(null)}
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clip-path="url(#clip0_41_1618)">
+                        <path
+                          d="M12.5486 11.0487L7.59857 15.9987L6.18457 14.5847L12.5486 8.2207L18.9126 14.5847L17.4986 15.9987L12.5486 11.0487Z"
+                          fill="white"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_41_1618">
+                          <rect
+                            width="24"
+                            height="24"
+                            fill="white"
+                            transform="translate(0.548828 0.220703)"
+                          />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  ) : (
+                    <svg
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clip-path="url(#clip0_41_1622)">
+                        <path
+                          d="M12.5486 13.3929L17.4986 8.44287L18.9126 9.85687L12.5486 16.2209L6.18457 9.85687L7.59857 8.44287L12.5486 13.3929Z"
+                          fill="white"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_41_1622">
+                          <rect
+                            width="24"
+                            height="24"
+                            fill="white"
+                            transform="translate(0.548828 0.220703)"
+                          />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  )}
+                </div>
+                {openQuestion === index && <p>{data.answer}</p>}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Gallery */}
+      <div className={styles.gallery}>
+        <h1>Our Gallery:</h1>
       </div>
     </div>
   );
